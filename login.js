8fbc6ea77df8b110f841f164bd899f5245d6f3a0
santoshi746 <111972093+santoshi746@users.noreply.github.com>
2023-06-14 10:05:50 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,10 @@
 function main(){
-    var name = document.getElementById("name");
-var number = document.getElementById("number");
 var email = document.getElementById("email");
 var password = document.getElementById("password");
-var username = document.getElementById("username");
-    if(name.value==""|| number.value==""|| email.value==""||password.value==""||username.value==""){
+    if(email.value==""||password.value==""){
         alert("Every credentials are mandatory");
     }else{
         alert("Welcome to Space interior designs");
     }
 }
+
